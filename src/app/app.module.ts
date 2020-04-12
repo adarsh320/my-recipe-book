27074c158d7b03c1078bd55f00b801df1a5a46df
adarsh320 <adarshshivhare567@gmail.com>
2020-04-12 18:52:42 +0530

    new components added recipes, recipe-list and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
